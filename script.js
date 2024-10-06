73e936f277ef2c7620bcea9ae5a0ea4f1ce4c24a
tamaraboruzs <tamara.boruzs@gmail.com>
2024-10-06 15:42:18 +0200

    fix login bug

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-document.getElementById("link").addEventListener("click", function() {
-    
-    var result = confirm("Átlépés a facebook oldalamra:");
-    if (result) {
-        
-        window.open = "https://www.facebook.com/tamara.boruzs/"; 
-    } 
-    else {
-      
-        console.log("Átirányítás lemondva."); 
+function openFacebookPage(){
+    var facebookUrl = "https://www.facebook.com/tamara.boruzs/";
+    var confirmation= confirm("Biztos el szeretne menni a Facebook oldalra?");
+
+    if(confirmation){
+        windows.open(facebookUrl, '_blank');
     }
-});
+    else{
+        alert("A facebook oldal nem lett megnyitva!");
+    }
+}
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
